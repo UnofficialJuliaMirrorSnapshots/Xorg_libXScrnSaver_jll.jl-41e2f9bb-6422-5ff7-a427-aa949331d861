6de39d707d7b16fd2c228a24a388257cd989b8fb
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-10-30 17:53:23 +0000

    Snapshot of branch master taken on 2019-10-30T13:53:21.964-04:00 from "https://github.com/UnofficialJuliaMirror/Xorg_libXScrnSaver_jll.jl-41e2f9bb-6422-5ff7-a427-aa949331d861"

---
+++ src/Xorg_libXScrnSaver_jll.jl
@@ -0,0 +1,31 @@
+module Xorg_libXScrnSaver_jll
+using Pkg, Pkg.BinaryPlatforms, Pkg.Artifacts, Libdl
+import Base: UUID
+
+# We put these inter-JLL-package API values here so that they are always defined, even if there
+# is no underlying wrapper held within this JLL package.
+const PATH_list = String[]
+const LIBPATH_list = String[]
+
+# Load Artifacts.toml file
+artifacts_toml = joinpath(@__DIR__, "..", "Artifacts.toml")
+
+# Extract all platforms
+artifacts = Pkg.Artifacts.load_artifacts_toml(artifacts_toml; pkg_uuid=UUID("41e2f9bb-6422-5ff7-a427-aa949331d861"))
+platforms = [Pkg.Artifacts.unpack_platform(e, "Xorg_libXScrnSaver", artifacts_toml) for e in artifacts["Xorg_libXScrnSaver"]]
+
+# Filter platforms based on what wrappers we've generated on-disk
+platforms = filter(p -> isfile(joinpath(@__DIR__, "wrappers", triplet(p) * ".jl")), platforms)
+
+# From the available options, choose the best platform
+best_platform = select_platform(Dict(p => triplet(p) for p in platforms))
+
+# Silently fail if there's no binaries for this platform
+if best_platform === nothing
+    @debug("Unable to load Xorg_libXScrnSaver; unsupported platform $(triplet(platform_key_abi()))")
+else
+    # Load the appropriate wrapper
+    include(joinpath(@__DIR__, "wrappers", "$(best_platform).jl"))
+end
+
+end  # module Xorg_libXScrnSaver_jll
